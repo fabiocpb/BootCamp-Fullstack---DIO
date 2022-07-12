2352bd0a30feec6efcfeaa4d31076cac8fbba0da
fabiocpb <fabiocpb@gmail.com>
2022-07-12 14:45:19 -0300

    Intro a JavaScript

---
+++ intro-a-javascript/teste.js
@@ -0,0 +1,11 @@
+function returnEvenValues(array){
+    let evenNums = [];
+    for (let i = 0; i< array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log(evenNums);
+}
+
+returnEvenValues([1,2,3,4,6,13]);
