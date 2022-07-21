3d09b50c729fb882e6f8246a8674657f460df1b4
fabiocpb <fabiocpb@gmail.com>
2022-07-21 02:49:44 -0300

    "typescript"

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,1 +1,5 @@
-console.log('TypeScript');
+const input = document.getElementById("campo") as HTMLInputElement;
+input.addEventListener('campo', (Event) =>{
+    const i = Event.currentTarget as HTMLInputElement;
+    console.log(i.value);    
+});
